61a1e12a90eff0b5289211f9efa8127621e59abb
yongsean <88657089+yongsean@users.noreply.github.com>
2023-08-28 13:31:58 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cdsilkd4knvu.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "gweeyongsean-bucket"
+customregion = "us-east-1"
+
